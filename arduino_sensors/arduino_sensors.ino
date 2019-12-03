a538bfd2f6f1d36bd5499d38de284b310d481c0c
Brandon Chin Loy <bcl320@nyu.edu>
2019-12-03 14:22:36 +0400

    added commands

--- arduino_sensors/arduino_sensors.ino
+++ arduino_sensors/arduino_sensors.ino
@@ -3,57 +3,59 @@
 #define ADDR_1 0x10
 #define ADDR_2 0x20 // (Me)
 
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(); // Colour Sensor
-int greenBool = 0;
+Adafruit_TCS34725 tcs_R = Adafruit_TCS34725(); // Colour Sensor (Right)
+char command;
+int greenBool_L = 0; // Left Sensor
+int greenBool_R = 0; // Right Sensor
+
+enum { // Commands from Hub
+  COLOUR_L = 1,
+  COLOUR_R  = 2
+};
 
 void setup() {
+  /* Serial */
    Serial.begin(9600);
-
   /* I2C */
+  command = 0;
   Wire.begin(ADDR_2);
-    if (tcs.begin()) {
-    Serial.println("Found sensor");
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+  /* Colour Sensors */
+  if (tcs_R.begin()) {
+    Serial.println("TCS34725 (Right) detected.");
   } else {
-    Serial.println("No TCS34725 found ... check your connections");
+    Serial.println("TCS34725 (Right) not detected.");
     while (1);
   }
 }
 
 void loop() {
-
-  uint16_t r, g, b, c, colorTemp, lux,X;
+  /* Colour Sensor (Left) */
+  
+  /* Colour Sensor (Right) */
+  uint16_t r, g, b, c;
   float average, red, green, blue;
-  tcs.getRawData(&r, &g, &b, &c);
+  tcs_R.getRawData(&r, &g, &b, &c);
   average= (r+g+b)/3;
   red= r/average;
   green= g/average;
   blue= b/average;
-  colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
-  lux = tcs.calculateLux(r, g, b);
-  if ((red<0.95) && (green>=1.3) && (blue<=0.95)) {
-    greenBool = 1;
+  if ((red < 0.95) && (green >= 1.3) && (blue <= 0.95)) {
+    greenBool_R = 1;
+    Serial.println("Green!");
   } else {
-    greenBool = 0;
-  }
-   if ( (red>1.4) && (green<0.9) &&(blue<0.9))
-  {
-    Serial.print ("red");
-  }
-  else if ((red<0.95) && (green>=1.3) && (blue<=0.95))
-  {
-    Serial.print ("green");
-    Serial.println();
-  }
-  else if ((red<0.8) && (green<1.2) && (blue>1.2))
-  {
-    Serial.print ("blue");
-  }
-  Wire.beginTransmission(ADDR_1);
-  Wire.write(greenBool);
-  int response = Wire.endTransmission();
-  if (response != 0) {
-    Serial.print("Error!\n");
-    Serial.print(response);
-    Serial.println();
+    greenBool_R = 0;
   }
 }
+
+void receiveEvent (int howMany) {
+  command = Wire.read();
+}
+
+void requestEvent() {
+  switch (command) {
+     case COLOUR_L: Wire.write(greenBool_L); break;   // send our ID 
+     case COLOUR_R: Wire.write(greenBool_R); break;  // send A0 value
+     }
+  }
