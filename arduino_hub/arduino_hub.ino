58006fcbb2f98d378a1a24b06e59f58880d2f048
Brandon Chin Loy <bcl320@nyu.edu>
2019-12-12 19:48:06 +0400

    made new additions to be tested

--- arduino_hub/arduino_hub.ino
+++ arduino_hub/arduino_hub.ino
@@ -1,3 +1,5 @@
+/* DEFINITIONS */
+
 #include <Wire.h> // I2C Library
 #include <ZumoShield.h> // Zumo Shield Library
 #define ADDR_1 0x10 // (Me)
@@ -17,6 +19,8 @@ ZumoMotors motors; // Motor Controls
 Pushbutton button(ZUMO_BUTTON); // Calibration Button
 int lastError = 0; // PID Variable
 const int MAX_SPEED = 140; // Maximum Speed
+
+/* SETUP */
 
 void setup() {
   /* Serial */
@@ -48,11 +52,17 @@ void setup() {
   while(buzzer.isPlaying()); // Necessary
 }
 
+/* MAIN LOOP */
+
 void loop() {
   /* Line Follower */
   unsigned int sensors[6];
   int position = reflectanceSensors.readLine(sensors);
   int error = position - 2500;
+  
+  // PLACEHOLDER
+  // If it detects white, just go straight until it is detected.
+  
   int speedDifference = error / 4 + 6 * (error - lastError);
   lastError = error;
   int m1Speed = MAX_SPEED + speedDifference;
@@ -70,12 +80,21 @@ void loop() {
   /* Read Colour Sensors */
   greenBool_L = readSensor(COLOUR_L, 1);
   greenBool_R = readSensor(COLOUR_R, 1);
+  if (greenBool_L == 1 || greenBool_R == 1) {
+    stopMove();
+    delay(50);
+    motors.setSpeeds(140, 140);
+    delay(50);
+    stopMove();
+    greenBool_L = readSensor(COLOUR_L, 1);
+    greenBool_R = readSensor(COLOUR_R, 1);
+  }
   
   /* Left Turn */
   if (greenBool_L == 1 && greenBool_R == 0) {
-    motors.setSpeeds(0, 0);
+    stopMove();
     int check_R = 0;
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 25; i++) {
       check_R = readSensor(COLOUR_R, 1);
       if (check_R == 1) {
         break;
@@ -83,20 +102,18 @@ void loop() {
     }
     if (check_R == 0) {
       Serial.println("I need to left turn.");
-      delay(100);
-      motors.setSpeeds(5, 140);
-      delay(2600);
-    } else {
+      turnMove(0, 260);
+    } else if (check_R == 1) {
       Serial.println("I need to turn around.");
-      reverseMove();
+      reverseMove(170);
     }
   }
   
   /* Right Turn */
   if (greenBool_L == 0 && greenBool_R == 1) {
-    motors.setSpeeds(0, 0);
+    stopMove();
     int check_L = 0;
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 25; i++) {
       check_L = readSensor(COLOUR_L, 1);
       if (check_L == 1) {
         break;
@@ -104,30 +121,47 @@ void loop() {
     }
     if (check_L == 0) {
       Serial.println("I need to right turn.");
-      delay(100);
-      motors.setSpeeds(140, 5);
-      delay(2600);
-    } else {
+      turnMove(1, 260);
+    } else if (check_L == 1) {
       Serial.println("I need to turn around.");
-      reverseMove();
+      reverseMove(170);
     }
   }
 
   /* Turn Around */
   if (greenBool_L == 1 && greenBool_R == 1) {
     Serial.println("I need to turn around.");
-    reverseMove();
+    reverseMove(170);
+  }  
+}
+
+/* FUNCTIONS */
+
+void turnMove(int signal, int turnValue) { // ~90° Movement
+  int m1Speed, m2Speed;
+  if (signal == 0) {
+    m1Speed = 0;
+    m2Speed = turnValue;
+  } else if (signal == 1) {
+    m1Speed = turnValue;
+    m2Speed = 0;
   }
+  motors.setSpeeds(m1Speed, m2Speed);
+  delay(800);
 }
 
-void reverseMove() { // MAX_SPEED = 140
-  motors.setSpeeds(-140, 0);
-  delay(3100);
-  motors.setSpeeds(0, 140);
-  delay(2550);
+void reverseMove(int turnValue) { // ~180° Movement
+  motors.setSpeeds(-turnValue, 0);
+  delay(2600);
+  motors.setSpeeds(0, turnValue);
+  delay(2100);
 }
 
-int readSensor(const byte command, const int responseSize) { 
+void stopMove() { // Stop Motors
+  motors.setSpeeds(0, 0);
+}
+
+int readSensor(const byte command, const int responseSize) { // Read from I2C
   int value = 0;
   Wire.beginTransmission(ADDR_2);
   Wire.write(command);
