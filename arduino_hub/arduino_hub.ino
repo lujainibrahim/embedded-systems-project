05e299f0f40d55c54909a43d640877e3f8e1c12d
Brandon Chin Loy <bcl320@nyu.edu>
2019-12-16 16:58:04 +0400

    added new test

--- arduino_hub/arduino_hub.ino
+++ arduino_hub/arduino_hub.ino
@@ -4,35 +4,86 @@
 #include <ZumoShield.h> // Zumo Shield Library
 #define ADDR_1 0x10 // (Me)
 #define ADDR_2 0x20
+#define TURN_BASE_SPEED 100 // Base speed when turning
+#define CALIBRATION_SAMPLES 70  // Number of compass readings to take when calibrating
+#define CRB_REG_M_2_5GAUSS 0x60 // CRB_REG_M value for magnetometer +/-2.5 gauss full scale
+#define CRA_REG_M_220HZ    0x1C // CRA_REG_M value for magnetometer 220 Hz update rate
+#define DEVIATION_THRESHOLD 5 // Allowed deviation relative to target angle that must be achieved
 
+/* Color Sensors */
 int greenBool_L = 0; // Received over I2C
 int greenBool_R = 0; // Received over I2C
-
 enum { // Commands to Sensors
   COLOR_L = 1,
   COLOR_R  = 2
 };
 
-ZumoBuzzer buzzer; // Not Used
-ZumoReflectanceSensorArray reflectanceSensors; // Reflectance Sensor
-ZumoMotors motors; // Motor Controls
-Pushbutton button(ZUMO_BUTTON); // Calibration Button
-int lastError = 0; // PID Variable
-const int MAX_SPEED = 140; // Maximum Speed
+/* Zumo Shield */
+ZumoBuzzer buzzer;
+ZumoReflectanceSensorArray reflectanceSensors;
+ZumoMotors motors;
+Pushbutton button(ZUMO_BUTTON);
+int MAX_SPEED = 160;
+
+/* Accelerometer */
+LSM303 compass;
+
+/* Ultrasonic Sensor */
+const int trigPin = A1;
+const int echoPin = 13;
+long duration, cm;
+
+/* PID Controller */
+int Kp = 8;
+int Kd = 4;
+int Ki = 1;
+int error = 0;
+int lastError = 0;
+int cumError = 0;
+
+/* Counters */
+int colorRead = 0; // Read Counter
+int loopTime = 0;
 
 /* S E T U P */
 
 void setup() {
+  /* Ultrasonic Sensor */
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  /* LED */
+  pinMode(6, OUTPUT);
   /* Serial */
-  Serial.begin(9600);
+//  Serial.begin(9600);
     /* I2C */
   Wire.begin(ADDR_1);
-  buzzer.play(">g32>>c32"); // Necessary
+  /* Accelerometer */
+  LSM303::vector<int16_t> running_min = {32767, 32767, 32767}, running_max = {-32767, -32767, -32767};
+  unsigned char index;
+  compass.init();
+  compass.enableDefault();
+  compass.writeReg(LSM303::CRB_REG_M, CRB_REG_M_2_5GAUSS); // +/- 2.5 Gauss Sensitivity
+  compass.writeReg(LSM303::CRA_REG_M, CRA_REG_M_220HZ); // 220 Hz Compass update rate
+  button.waitForButton();
+  motors.setSpeeds(200, -200);
+  for(index = 0; index < CALIBRATION_SAMPLES; index ++) {
+    compass.read();
+    running_min.x = min(running_min.x, compass.m.x);
+    running_min.y = min(running_min.y, compass.m.y);
+    running_max.x = max(running_max.x, compass.m.x);
+    running_max.y = max(running_max.y, compass.m.y);
+    delay(50);
+  }
+  motors.setSpeeds(0, 0);
+  compass.m_max.x = running_max.x;
+  compass.m_max.y = running_max.y;
+  compass.m_min.x = running_min.x;
+  compass.m_min.y = running_min.y;
   /* Zumo Shield */
-  reflectanceSensors.init(); // Initialize reflectance sensor
-  button.waitForButton(); // Wait for the user button to be pressed and released
-  pinMode(13, OUTPUT); // LED
-  digitalWrite(13, HIGH); // Turn on LED to indicate we are in calibration mode
+  buzzer.play(">g32>>c32"); // Necessary
+  reflectanceSensors.init();
+  button.waitForButton();
+  digitalWrite(6, HIGH); 
   delay(1000);
   int i;
   for(i = 0; i < 80; i++) {
@@ -44,22 +95,31 @@ void setup() {
     delay(20);
   }
   motors.setSpeeds(0,0);
-  digitalWrite(13, LOW); // Turn off LED to indicate we are through with calibration
+  digitalWrite(6, LOW);
   buzzer.play(">g32>>c32"); // Necessary
   Serial.println("Calibration complete.");
-  button.waitForButton(); // Wait for the user button to be pressed and released
+  button.waitForButton();
   buzzer.play("L16 cdegreg4"); // Necessary
   while(buzzer.isPlaying()); // Necessary
 }
 
-/* M A I N  L O O P */
+/* L O O P */
 
 void loop() {
   /* Line Follower */
+  loopTime++;
+  if (loopTime == 100) { 
+    // P L A C E H O L D E R
+    loopTime = 0;
+  }
+  if (loopTime % 2 == 0) {
+    cumError = 0;
+  }
   unsigned int sensors[6];
   int position = reflectanceSensors.readLine(sensors);
-  int error = position - 2500;
-  int speedDifference = error / 4 + 6 * (error - lastError);
+  error = position - 2500;
+  cumError += error; 
+  int speedDifference = Kp*error + Kd*(error - lastError) + Ki*cumError;
   lastError = error;
   int m1Speed = MAX_SPEED + speedDifference;
   int m2Speed = MAX_SPEED - speedDifference;
@@ -73,72 +133,136 @@ void loop() {
     m2Speed = MAX_SPEED;
   motors.setSpeeds(m1Speed, m2Speed);
   
-  /* Read Color Sensors */
-  greenBool_L = readSensor(COLOR_L, 1);
-  greenBool_R = readSensor(COLOR_R, 1);
-  
-  /* Left Turn */
-  if (greenBool_L == 1 && greenBool_R == 0) {
-    // To Be Calibrated
-    stopMove();
-    while(1);
+  /* Color Sensors Check */
+  colorRead++;
+  if (colorRead == 50) {
+    greenBool_L = readSensor(COLOR_L, 1);
+    greenBool_R = readSensor(COLOR_R, 1);
+    /* Left Turn */
+    if (greenBool_L == 1 && greenBool_R == 0) {
+      int check = colorConfirm(COLOR_R);
+      if (check == 0) {
+        // P L A C E H O L D E R
+        turnAngle(-90);
+      } else if (check == 1) {
+        // P L A C E H O L D E R
+        turnAngle(180);
+      }
+    }
+    /* Right Turn */
+    if (greenBool_L == 0 && greenBool_R == 1) {
+      int check = colorConfirm(COLOR_L);
+      if (check == 0) {
+        // P L A C E H O L D E R
+        turnAngle(90);
+      } else if (check == 1) {
+        // P L A C E H O L D E R
+        turnAngle(180);
+      }
+    }  
+    /* Turn Around */
+    if (greenBool_L == 1 && greenBool_R == 1) {
+      // P L A C E H O L D E R
+      turnAngle(180);
+    }
+    /* Reset Counter */
+    colorRead = 0;
   }
-  
-  /* Right Turn */
-  if (greenBool_L == 0 && greenBool_R == 1) {
-    // To Be Calibrated
-    stopMove();
-    while(1);
-  }
-
-  /* Turn Around */
-  if (greenBool_L == 1 && greenBool_R == 1) {
-    // To Be Calibrated
-    stopMove();
-    while(1);
-  }  
 }
 
 /* F U N C T I O N S */
 
-void turnMove(int signal, int turnValue) { // ~90° Movement
-  int m1Speed, m2Speed;
-  if (signal == 0) {
-    m1Speed = 0;
-    m2Speed = turnValue;
-  } else if (signal == 1) {
-    m1Speed = turnValue;
-    m2Speed = 0;
-  }
-  motors.setSpeeds(m1Speed, m2Speed);
-  delay(800);
-}
+/* Hard-Coded Movements */
 
-void reverseMove(int turnValue) { // ~180° Movement
-  motors.setSpeeds(-turnValue, 0);
-  delay(2600);
-  motors.setSpeeds(0, turnValue);
-  delay(2100);
-}
-
-void stopMove() { // Stop Motors
+void stopMove() {
   motors.setSpeeds(0, 0);
 }
+
+/* Color Sensors */
 
 int readSensor(const byte command, const int responseSize) { // Read from I2C
   int value = 0;
   Wire.beginTransmission(ADDR_2);
   Wire.write(command);
-  int status = Wire.endTransmission();
-  if (status != 0) {
-    digitalWrite(13, HIGH);
-    while(1); // Pause Arduino
-  } else {
-    digitalWrite(13, LOW);
-  }
+  int status = Wire.endTransmission(0);
   Wire.requestFrom(ADDR_2, responseSize, 1); 
   if (Wire.available() != 0) {
     value = Wire.read(); 
   }
   return value;
+}
+
+int colorConfirm(const byte command) {
+  stopMove();
+  int check = 0;
+  for (int i = 1; i < 11; i++) {
+    if (i % 2 == 0) { // Delay for I2C
+      check = readSensor(command, 1);
+      if (check == 1) {
+        break;
+      }
+    }
+  }
+  return check;
+}
+
+/* Accelerometer (Turns) */
+
+template <typename T> float heading(LSM303::vector<T> v) {
+  float x_scaled =  2.0*(float)(v.x - compass.m_min.x) / ( compass.m_max.x - compass.m_min.x) - 1.0;
+  float y_scaled =  2.0*(float)(v.y - compass.m_min.y) / (compass.m_max.y - compass.m_min.y) - 1.0;
+  float angle = atan2(y_scaled, x_scaled)*180 / M_PI;
+  if (angle < 0)
+    angle += 360;
+  return angle;
+}
+
+float relativeHeading(float heading_from, float heading_to) {
+  float relative_heading = heading_to - heading_from;
+  if (relative_heading > 180)
+    relative_heading -= 360;
+  if (relative_heading < -180)
+    relative_heading += 360;
+  return relative_heading;
+}
+
+float averageHeading() {
+  LSM303::vector<int32_t> avg = {0, 0, 0};
+  for(int i = 0; i < 10; i ++) {
+    compass.read();
+    avg.x += compass.m.x;
+    avg.y += compass.m.y;
+  }
+  avg.x /= 10.0;
+  avg.y /= 10.0;
+  return heading(avg);
+}
+
+void turnAngle(int angle) {
+  int check = 0;
+  while(1) {
+    float heading, relative_heading;
+    int speed;
+    static float target_heading = averageHeading();
+    heading = averageHeading();
+    relative_heading = relativeHeading(heading, target_heading);
+    if(abs(relative_heading) < DEVIATION_THRESHOLD) {
+      if (check == 0) {
+        stopMove();
+        delay(100);
+        target_heading = fmod(averageHeading() + angle, 360);
+        check = 1;
+      } else {
+        stopMove();
+        break;
+      }
+    } else {
+      speed = MAX_SPEED*relative_heading/180;
+      if (speed < 0)
+        speed -= TURN_BASE_SPEED;
+      else
+        speed += TURN_BASE_SPEED;
+      motors.setSpeeds(speed, -speed);
+    }
+  }
 }
---
+++ individual_tests/Test_MUX_3/Test_MUX_3_2_2/Test_MUX_3_2_2.ino
@@ -0,0 +1,109 @@
+/* D E F I N I T I O N S */
+
+#include <Wire.h> // I2C Library
+#include <Adafruit_TCS34725.h> // TCS34725 Library
+#define ADDR_1 0x10
+#define ADDR_2 0x20 // (Me)
+#define TCAADDR 0x70
+
+/* I2C */
+enum { // Commands from Hub
+  COLOR_L = 1,
+  COLOR_R  = 2
+};
+
+Adafruit_TCS34725 tcs_L = Adafruit_TCS34725(); // Colour Sensor (Left)
+Adafruit_TCS34725 tcs_R = Adafruit_TCS34725(); // Colour Sensor (Right)
+char command;
+int value_L = 0; // Left Sensor
+int value_R = 0; // Right Sensor
+
+/* S E T U P */
+
+void setup() {
+  pinMode(13, OUTPUT);
+  /* Serial */
+//   Serial.begin(9600);
+  /* I2C */
+  command = 0;
+  Wire.begin(ADDR_2);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+  /* Colour Sensors */
+  tcaselect(1); // Left Sensor
+  if (tcs_L.begin()) {
+//    Serial.println("TCS34725 (Left) detected.");
+  } else {
+//    Serial.println("TCS34725 (Left) not detected.");
+    while(1);
+  }
+  tcaselect(2); // Right Sensor
+  if (tcs_R.begin()) {
+//    Serial.println("TCS34725 (Right) detected.");
+  } else {
+//    Serial.println("TCS34725 (Right) not detected.");
+    while(1);
+  }
+}
+
+void loop() {
+  /* Color Sensor (Left) */
+  tcaselect(1);
+  uint16_t r_L, g_L, b_L, c_L, lux_L;
+  float average_L, red_L, green_L, blue_L;
+  tcs_L.getRawData(&r_L, &g_L, &b_L, &c_L);
+  lux_L = tcs_L.calculateLux(r_L, g_L, b_L);
+  average_L = (r_L+g_L+b_L)/3;
+  red_L = r_L/average_L;
+  green_L = g_L/average_L;
+  blue_L = b_L/average_L;
+  if ((red_L < 0.95) && (green_L >= 1.3) && (blue_L <= 0.95)) {
+    value_L = 1;
+  } else if ((red_L >= 1) && (green_L < 1.2) && (blue_L >= 0.8) && (lux_L <= 50)) {
+    value_L = 2;
+  } else {
+    value_L = 0;
+  }
+  /* Color Sensor (Right) */
+  tcaselect(2);
+  uint16_t r_R, g_R, b_R, c_R, lux_R;
+  float average_R, red_R, green_R, blue_R;
+  tcs_R.getRawData(&r_R, &g_R, &b_R, &c_R);
+  lux_R = tcs_R.calculateLux(r_R, g_R, b_R);
+  average_R = (r_R+g_R+b_R)/3;
+  red_R = r_R/average_R;
+  green_R = g_R/average_R;
+  blue_R = b_R/average_R;
+  if ((red_R <= 1.05) && (green_R >= 1.3) && (blue_R <= 0.95)) {
+    value_R = 1;
+  } else if ((red_R >= 1) && (green_R < 1.2) && (blue_R >= 0.8) && (lux_R <= 50)) {
+    value_R = 2;
+  } else {
+    value_R = 0;
+  }
+  if (value_L == 1 && value_R == 1) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+}
+
+/* F U N C T I O N S */
+
+void receiveEvent (int howMany) {
+  command = Wire.read();
+}
+
+void requestEvent() {
+  switch (command) {
+     case COLOR_L: Wire.write(value_L); break;
+     case COLOR_R: Wire.write(value_R); break;
+  }
+}
+
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
